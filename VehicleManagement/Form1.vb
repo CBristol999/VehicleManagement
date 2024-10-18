516925caeda9b968a04da3ed335acf0a76c62150
Christian Bristol <158741617+CBristol999@users.noreply.github.com>
2024-10-18 11:40:36 +0800

    Add files via upload

---
+++ VehicleManagement/Form1.vb
@@ -0,0 +1,38 @@
+﻿Imports MySql.Data.MySqlClient
+
+Public Class Form1
+    Dim query As String
+    Dim tableinfo As DataTable 'hold record from mysql
+    Dim da As MySqlDataAdapter 'handle sql query
+
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        'call databaseConnect procudure to open connection
+        databaseConnect()
+    End Sub
+
+    Private Sub btnclose_Click(sender As Object, e As EventArgs) Handles btnclose.Click
+        End
+    End Sub
+
+    Private Sub btnload_Click(sender As Object, e As EventArgs) Handles btnload.Click
+        Try
+            query = "SELECT * FROM vehicleInfo"
+            'send sql query to server
+            da = New MySqlDataAdapter(query, datacon)
+            tableinfo = New DataTable
+            'fill the table
+            da.Fill(tableinfo)
+            'display record in data grid view
+            dataRecord.DataSource = tableinfo
+
+            'autoresize column width
+            dataRecord.AutoSizeColumnsMode = DataGridViewAutoSizeColumnsMode.Fill
+
+            datacon.Close()
+        Catch ex As Exception
+            MsgBox(ex.Message)
+            datacon.Close()
+        End Try
+    End Sub
+End Class
---
+++ VehicleManagement/Module1.vb
@@ -0,0 +1,29 @@
+﻿Imports MySql.Data.MySqlClient
+
+Module Module1
+    'declare variable
+    Public datacon As New MySqlConnection
+    Dim server As String = "127.0.0.1"
+    Dim dbasename As String = "cs2adbase"
+    Dim username As String = "root"
+    Dim password As String = "admin"
+
+    'create a connection subprocedure
+    Public Sub databaseConnect()
+        'check if connection exist
+        If Not datacon Is Nothing Then
+            'establish connection
+            datacon.ConnectionString = "server=" & server & ";user id=" & username & ";password=" & password & ";database=" & dbasename & ""
+
+
+            Try
+                'open connection
+                datacon.Open()
+                MsgBox("'database connected")
+            Catch ex As Exception
+                MsgBox(ex.Message)
+            End Try
+        End If
+    End Sub
+
+End Module
